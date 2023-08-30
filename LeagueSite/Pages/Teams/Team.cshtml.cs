e911a4c1a288fcb2c3b83d28ecc852455f0080f0
AndyJL1999 <andylainez115@hotmail.com>
2023-08-30 19:58:25 -0400

    --Team page now displays team's related players

--- LeagueSite/Pages/Teams/Team.cshtml.cs
+++ LeagueSite/Pages/Teams/Team.cshtml.cs
@@ -2,6 +2,7 @@ using LeagueSite.Data;
 using LeagueSite.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace LeagueSite.Pages.Teams
 {
@@ -16,11 +17,16 @@ namespace LeagueSite.Pages.Teams
 
         public Team Team { get; set; }
         public string LogoUrl { get; set; }
+        public List<Player> RelatedPlayers { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
             Team = await _context.Teams.FindAsync(id);
             LogoUrl = Team.TeamId + ".png";
+            RelatedPlayers = await _context.Players
+                .Where(p => p.TeamId == id)
+                .OrderBy(p => p.Name)
+                .ToListAsync();
 
             return Page();
         }
