fd540485cebee980d7d4e612f1d33e87679b525e
AndyJL1999 <andylainez115@hotmail.com>
2023-08-30 19:57:01 -0400

    --Player page now shows all players --Sorting features for player page now implemented

--- LeagueSite/Pages/Players/Index.cshtml.cs
+++ LeagueSite/Pages/Players/Index.cshtml.cs
@@ -2,6 +2,7 @@ using LeagueSite.Data;
 using LeagueSite.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeagueSite.Pages.Players
@@ -16,10 +17,50 @@ namespace LeagueSite.Pages.Players
         }
 
         public List<Player> Players { get; set; }
+        public SelectList Teams { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SortField { get; set; } = "Name";
+        [BindProperty(SupportsGet = true)]
+        public string SelectedTeam { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
-            Players = await _context.Players.ToListAsync();
+            Players = await _context.Players
+                .OrderBy(p => p.Position)
+                .ToListAsync();
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                Players = Players
+                    .Where(c => c.Name.Contains(SearchString))
+                    .ToList();
+            }
+
+            switch (SortField)
+            {
+                case "TeamId":
+                    Players = Players.OrderBy(p => p.TeamId).ToList();
+                    break;
+                case "Name":
+                    Players = Players.OrderBy(p => p.Name).ToList();
+                    break;
+                case "Position":
+                    Players = Players.OrderBy(c => c.Position).ToList();
+                    break;
+            }
+
+            if (!string.IsNullOrEmpty(SelectedTeam))
+            {
+                Players = Players.Where(c => c.TeamId == SelectedTeam).ToList();
+            }
+
+            Teams = new SelectList(await _context.Teams
+                .OrderBy(t => t.TeamId)
+                .Select(t => t.TeamId)
+                .ToListAsync());
 
             return Page();
         }
