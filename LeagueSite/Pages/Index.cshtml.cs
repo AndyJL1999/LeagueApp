5262d21017a2333982589c5c149c57b27c37fc70
AndyJL1999 <andylainez115@hotmail.com>
2023-09-05 21:02:00 -0400

    --Home page now displays NFL as base league

--- LeagueSite/Pages/Index.cshtml.cs
+++ LeagueSite/Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LeagueSite.Data;
+using LeagueSite.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace LeagueSite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly LeagueContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, LeagueContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public League League { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            League = await _context.Leagues.FirstOrDefaultAsync();
         }
     }
 }
